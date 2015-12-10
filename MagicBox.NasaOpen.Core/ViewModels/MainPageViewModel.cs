c1a15831bc962b2889b5273b3f8036d7826615b9
Rafael da Silva Ferreira <rafael_yami@hotmail.com>
2015-12-10 21:39:26 -0200

    Commit adding the primary support for Prism with Unity on ViewModel; Also, in this commit it added the unit test framework by Microsoft.

---
+++ MagicBox.NasaOpen.Core/ViewModels/MainPageViewModel.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Prism.Windows.Mvvm;
+
+namespace MagicBox.NasaOpen.Core.ViewModels
+{
+    public sealed class MainPageViewModel : ViewModelBase, IMainPageViewModel
+    {
+    }
+}
---
+++ MagicBox.NasaOpen/Views/MainPage.xaml.cs
@@ -0,0 +1,16 @@
+﻿namespace MagicBox.NasaOpen.Views
+{
+    /// <summary>
+    /// The code-behind for the Main page.
+    /// </summary>
+    public sealed partial class MainPage
+    {
+        /// <summary>
+        /// Initializes page's components.
+        /// </summary>
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
